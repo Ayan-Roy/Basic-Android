15ed85badf27e191ab5876889a84dd2a207312d5
Ayan Roy <51925552+Ayan-Roy@users.noreply.github.com>
2020-03-31 12:06:17 +0600

    Create Simple ListView.java

---
+++ JAVA/Simple ListView.java
@@ -0,0 +1,39 @@
+package com.ayan.one;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.GridView;
+import android.widget.ListView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    ListView listView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        listView = findViewById(R.id.lv);
+
+        final String[] items= {"A", "B", "C", "D", "E"};
+
+        ArrayAdapter arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, items);
+        listView.setAdapter(arrayAdapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(MainActivity.this, "You have clicked: "+items[position], Toast.LENGTH_SHORT).show(); }
+        });
+
+
+
+    }
+}
